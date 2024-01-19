36e8cc991c8e83c31766eb6ed4ebb7ab522aea3f
alvin-c-cruz <alvinccruz12@gmail.com>
2024-01-19 20:07:55 +0000

    Configured flask_app.py for pythonanywhere

--- flask_app.py
+++ flask_app.py
@@ -3,13 +3,14 @@ import ledger, payable_payment, taxes
 from pathlib import Path
 
 root = Path.cwd()
-instance_folder = root / "instance"
-template_folder = root / "templates"
+print("Root Folder: ", root)
+instance_folder = root / "toshco_inc" / "instance"
+template_folder = root / "toshco_inc" / "templates"
 blueprint_groups = (ledger, payable_payment, taxes)
 
 app = create_app(
-    instance_folder=instance_folder, 
-    template_folder=template_folder, 
+    instance_folder=instance_folder,
+    template_folder=template_folder,
     blueprint_groups=blueprint_groups
     )
 
