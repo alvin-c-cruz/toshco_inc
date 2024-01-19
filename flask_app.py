51b5c3f00650dd352bffc23324d7835cbf351d0f
alvin-c-cruz <alvinccruz12@gmail.com>
2024-01-20 04:41:03 +0800

    Configure flask_app.py for development

--- flask_app.py
+++ flask_app.py
@@ -3,16 +3,30 @@ import ledger, payable_payment, taxes
 from pathlib import Path
 
 root = Path.cwd()
-print("Root Folder: ", root)
+
+# Pythonanywhere
 instance_folder = root / "toshco_inc" / "instance"
 template_folder = root / "toshco_inc" / "templates"
 blueprint_groups = (ledger, payable_payment, taxes)
 
-app = create_app(
-    instance_folder=instance_folder,
-    template_folder=template_folder,
-    blueprint_groups=blueprint_groups
-    )
+try:
+    app = create_app(
+        instance_folder=instance_folder,
+        template_folder=template_folder,
+        blueprint_groups=blueprint_groups
+        )
+except:
+    pass
 
 if __name__ == "__main__":
+    #  For development
+    instance_folder = root / "instance"
+    template_folder = root /  "templates"
+
+    app = create_app(
+        instance_folder=instance_folder,
+        template_folder=template_folder,
+        blueprint_groups=blueprint_groups
+        )
+
     app.run()
